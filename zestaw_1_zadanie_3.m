e598b0eb28353b3deda2aa7c62700b0245aaf10d
Nukya <53628577+Nukya@users.noreply.github.com>
2025-10-06 22:41:47 +0200

    chyba koniec zadania 3

--- zestaw_1_zadanie_3.m
+++ zestaw_1_zadanie_3.m
@@ -1,4 +1,5 @@
 clc
+clear
 
 syms C i R
 
@@ -10,24 +11,25 @@ Pr=diff(P, R);
 Pi=diff(P, i);
 
 % surowe dane
-rawC = 1.1; %mF
-rawR = 1; %kOhm
-rawI = 1.2; %mA
+rawC = 1.1;     %mF
+rawR = 1;       %kOhm
+rawI = 1.2;     %mA
 
 % dane zamienione na podstawowe jednostki SI
-aC = 1.1 * 10^(-3);
-aR = 1 *  10^3;
-ai = 1.2 * 10^(-3);
+aC = 1.1 * 10^(-3);     %F
+aR = 1 *  10^3;         %Ohm
+ai = 1.2 * 10^(-3);     %A
 
 % błąd argumentu
-deltaC = 0.05 * rawC;
-deltaR = 0.02 * rawR;
-deltaI = 0.01 * rawI;
+deltaC = 0.05 * aC;
+deltaR = 0.02 * aR;
+deltaI = 0.01 * ai;
 
 PcPodstawione = subs(Pc,{C, i, R},{aC, ai, aR});
 PrPodstawione = subs(Pr,{C, i, R},{aC, ai, aR});
 PiPodstawione = subs(Pi,{C, i, R},{aC, ai, aR});
 
+PPodstawione = subs(P,{C, i, R},{aC, ai, aR});
 
 % podstawienie do wzoru na błąd bezwzględny funkcji
 bladBezwzglednyP = abs(PrPodstawione) * deltaR + abs(PiPodstawione) * deltaI + abs(PcPodstawione) * deltaC;
@@ -37,4 +39,5 @@ bladBezwzglednyP = abs(PrPodstawione) * deltaR + abs(PiPodstawione) * deltaI + a
 bladWzglednyP = (bladBezwzglednyP)/(abs(PPodstawione));
 
 
-% aaa
+fprintf('Blad bezwzgledny funkcji wynosi: %d\n', double(bladBezwzglednyP));
+fprintf('Blad wzgledny funkcji wynosi: %d\n', double(bladWzglednyP));
