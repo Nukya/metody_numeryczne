dc0b6553c109151778466ca8614bcfb96c0dd47b
Nukya <53628577+Nukya@users.noreply.github.com>
2025-11-30 14:16:33 +0100

    podrecznik

--- projekt/projekt_chlodzenie_cz2.m
+++ projekt/projekt_chlodzenie_cz2.m
@@ -256,5 +256,3 @@ function [DeltaT_pom, h_pom] = pobierz_dane_pomiarowe()
 DeltaT_pom = [-1500 -1000 -300 -50 -1 1 20 50 200 400 1000 2000];
 h_pom      = [178   176   168  161 160 160 160.2 161 165 168 174 179];
 end
-
-end
